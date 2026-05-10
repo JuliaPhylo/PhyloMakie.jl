2732d7e637a477ccf9e50e87ea465dec7bb2c715
Jeet Sukumaran <jeetsukumaran@gmail.com>
2026-05-10 00:34:40 -0700

    Tranche 3 implementation

--- src/PhyloMakie.jl
+++ src/PhyloMakie.jl
@@ -2,6 +2,8 @@ module PhyloMakie
 
 include("keyword_contract.jl")
 include("keyword_normalization.jl")
+include("layout_engine.jl")
+include("annotation_data.jl")
 include("verification_foundation.jl")
 
 end
