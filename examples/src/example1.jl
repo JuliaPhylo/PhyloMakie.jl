26e2e1bbe8131aa72d7a3aa408b5c62bb7fdcca6
Jeet Sukumaran <jeetsukumaran@gmail.com>
2026-05-10 21:17:01 -0700

    Add basic example

---
+++ examples/src/example1.jl
@@ -0,0 +1,15 @@
+using CairoMakie
+using PhyloMakie
+using PhyloNetworks: readnewick
+
+net = readnewick(
+    "(((A:.2,(B:.1)#H1:.1::0.9):.1,(C:.11,#H1:.01::0.1):.19):.1,D:.4);",
+)
+
+plot(
+    net;
+    use_edge_lengths = true,
+    show_gamma = true,
+    show_tip_labels = true,
+    style = :fulltree,
+)
